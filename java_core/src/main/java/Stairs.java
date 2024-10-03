336aefcb71b0bed32366dc3ae425c660ba63d623
anastasiiiii <nastia262002@gmail.com>
2024-10-03 21:58:03 +0200

    add function

---
+++ java_core/src/main/java/Stairs.java
@@ -0,0 +1,17 @@
+public class Stairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(4));
+        System.out.println(climbStairs(5));
+    }
+
+    public static int climbStairs(int n) {
+        int sum = 0; //завжди буде метод з 1
+        while(n > 1) {
+            n = n - 2;
+            sum++;
+        }
+        sum = 2*sum + 1;
+        return sum;
+    }
+}
