985afe3567876ac514b538e9cf7e338d11dc6237
anastasiiiii <nastia262002@gmail.com>
2024-10-09 18:19:13 +0200

    find duplicates task3

--- java_core/src/main/java/HashTable/Duplicates.java
+++ java_core/src/main/java/HashTable/Duplicates.java
@@ -10,8 +10,15 @@ public class Duplicates {
         int[] nums = {1, 2, 3, 1};
         int[] nums2 = {1, 2, 3, 4};
         int[] nums3 = {1, 2, 3, 1, 2, 3};
+        int[] nums4 = {1, 5, 9, 1, 5, 9};
+        int[] nums5 = {8, 7, 15, 1, 6, 1, 9, 15};
         int k = 3;
         int k1 = 2;
+        int indexDiff = 3;
+        int valueDiff = 0;
+        int indexDiff1 = 2;
+        int indexDiff2 = 1;
+        int valueDiff1 = 3;
         System.out.println(containsDuplicate(nums));
         System.out.println(containsDuplicate(nums2));
         System.out.println("***********************************");
@@ -20,6 +27,10 @@ public class Duplicates {
         System.out.println("************************************");
         System.out.println(containsNearbyDuplicate2(nums, k));
         System.out.println(containsNearbyDuplicate2(nums3, k1));
+        System.out.println("************************************");
+        System.out.println(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff));
+        System.out.println(containsNearbyAlmostDuplicate(nums4, indexDiff1, valueDiff1));
+        System.out.println(containsNearbyAlmostDuplicate(nums5, indexDiff2, valueDiff1));
     }
 
     public static boolean containsDuplicate(int[] nums) {
@@ -69,4 +80,33 @@ public class Duplicates {
         return false;
     }
 
+    /*
+    You are given an integer array nums and two integers indexDiff and valueDiff.
+    Find a pair of indices (i, j) such that:
+    i != j,
+    abs(i - j) <= indexDiff.
+    abs(nums[i] - nums[j]) <= valueDiff, and Return true if such pair exists or false otherwise.
+     */
+
+    public static boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int previousIndex = map.get(nums[i]);
+                if(i != previousIndex){
+                    int j = Math.abs(i - previousIndex);
+                    if(j <= indexDiff){
+                        int k = Math.abs(nums[i] - nums[previousIndex]);
+                        if(k <= valueDiff){
+                            return true;
+                        }
+                    }
+                }
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
 }
