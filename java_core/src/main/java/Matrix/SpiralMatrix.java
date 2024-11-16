99ccbde02a71fe483ba2aeee7dcb18af451a26d9
anastasiiiii <nastia262002@gmail.com>
2024-11-16 07:32:38 +0100

    add a method to find the longest palindrome substring and correct the method that is used to find spiral matrix

--- java_core/src/main/java/Matrix/SpiralMatrix.java
+++ java_core/src/main/java/Matrix/SpiralMatrix.java
@@ -16,31 +16,35 @@ public class SpiralMatrix {
 
     private static List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> spiralMatrix = new ArrayList<>();
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return spiralMatrix;
         }
 
         int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
 
-        while(top <= bottom && left <= right){
+        while (top <= bottom && left <= right) {
+            // Прохід зліва направо по верхньому рядку
             for (int i = left; i <= right; i++) {
                 spiralMatrix.add(matrix[top][i]);
             }
             top++;
 
+            // Прохід зверху вниз по правому стовпцю
             for (int i = top; i <= bottom; i++) {
                 spiralMatrix.add(matrix[i][right]);
             }
-
             right--;
 
-            if(top <= bottom){
-                for (int i = right; i >= left ; i--) {
+            // Прохід справа наліво по нижньому рядку
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
                     spiralMatrix.add(matrix[bottom][i]);
                 }
+                bottom--;
             }
 
-            if(left <= right){
+            // Прохід знизу вверх по лівому стовпцю
+            if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     spiralMatrix.add(matrix[i][left]);
                 }
@@ -50,4 +54,5 @@ public class SpiralMatrix {
 
         return spiralMatrix;
     }
+
 }
