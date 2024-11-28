d51127b90e80444cd813d63c40ed26f448e57388
anastasiiiii <nastia262002@gmail.com>
2024-11-28 14:02:44 +0100

    add find next prime number method

--- java_core/src/main/java/Math/PrimeNumber.java
+++ java_core/src/main/java/Math/PrimeNumber.java
@@ -4,20 +4,28 @@ public class PrimeNumber {
     //A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
     public static void main(String[] args) {
         System.out.println(isPrime(23));
-
+        System.out.println(findNextPrime(23));
     }
 
-    private static boolean isPrime(int number){
-        if(number <= 1){
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
             return false;
         }
 
-        for(int i = 2; i <= Math.sqrt(number); i++){
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             System.out.println(i);
-            if(number % i == 0){
+            if (number % i == 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static int findNextPrime(int n){
+        int m = n + 1;
+        while (!isPrime(m)){
+            m++;
+        }
+        return m;
     }
 }
