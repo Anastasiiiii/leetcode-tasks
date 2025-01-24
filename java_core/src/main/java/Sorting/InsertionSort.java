752daee00c959ed9b7875f557491f8b9f99cf792
anastasiiiii <nastia262002@gmail.com>
2025-01-24 22:38:19 +0100

    change merging method

---
+++ java_core/src/main/java/Sorting/InsertionSort.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 6, 1, 3};
+        System.out.println(Arrays.toString(insertionSortMethod(arr)));
+    }
+
+    //Time complexity: O(n^2)
+    private static int[] insertionSortMethod(int[] arr){
+        // TODO: implement the Insertion Sort algorithm
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if(arr[i] < arr[j]){
+                    swapMethod(arr, i, j);
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    private static void swapMethod(int[] arr, int firstIndex, int secondIndex){
+        int temp = arr[firstIndex];
+        arr[firstIndex] = arr[secondIndex];
+        arr[secondIndex] = temp;
+    }
+}
